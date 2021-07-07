1d28d2a251d69e3e63593a244607aeee8f29a3a9
Odedoyin Akindele <akindeleodedoyin@gmail.com>
2021-07-07 10:44:35 +0100

    created webscraper project

---
+++ src/main/java/com/pulga/springwebscraper/SpringwebscraperApplication.java
@@ -0,0 +1,13 @@
+package com.pulga.springwebscraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwebscraperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwebscraperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pulga/springwebscraper/SpringwebscraperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pulga.springwebscraper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwebscraperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
